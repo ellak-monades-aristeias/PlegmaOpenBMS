46f4725fab3e10b6ebc252628115f58392fae832
Manolis N <nikil511@gmail.com>
2014-12-03 18:18:53 +0200

    SMA Sunnny web box binder

---
+++ bundles/binding/org.openhab.binding.sunnywebbox/src/main/java/org/openhab/binding/sunnywebbox/internal/SunnyWebBoxGenericBindingProvider.java
@@ -0,0 +1,116 @@
+/**
+ * openHAB, the open Home Automation Bus.
+ * Copyright (C) 2010-2013, openHAB.org <admin@openhab.org>
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Additional permission under GNU GPL version 3 section 7
+ *
+ * If you modify this Program, or any covered work, by linking or
+ * combining it with Eclipse (or a modified version of that library),
+ * containing parts covered by the terms of the Eclipse Public License
+ * (EPL), the licensors of this Program grant you additional permission
+ * to convey the resulting work.
+ */
+package org.openhab.binding.sunnywebbox.internal;
+
+import java.util.List;
+
+import org.openhab.binding.sunnywebbox.SunnyWebBoxBindingProvider;
+import org.openhab.core.binding.BindingConfig;
+import org.openhab.core.items.Item;
+import org.openhab.core.library.items.DimmerItem;
+import org.openhab.core.library.items.SwitchItem;
+import org.openhab.model.item.binding.AbstractGenericBindingProvider;
+import org.openhab.model.item.binding.BindingConfigParseException;
+
+
+/**
+ * This class is responsible for parsing the binding configuration.
+ * 
+ * @author johngouf
+ * @since 1.3.1
+ */
+public class SunnyWebBoxGenericBindingProvider extends AbstractGenericBindingProvider implements SunnyWebBoxBindingProvider {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public String getBindingType() {
+		return "sunnywebbox";
+	}
+
+	/**
+	 * @{inheritDoc}
+	 */
+	@Override
+	public void validateItemType(Item item, String bindingConfig) throws BindingConfigParseException {
+		//if (!(item instanceof SwitchItem || item instanceof DimmerItem)) {
+		//	throw new BindingConfigParseException("item '" + item.getName()
+		//			+ "' is of type '" + item.getClass().getSimpleName()
+		//			+ "', only Switch- and DimmerItems are allowed - please check your *.items configuration");
+		//}
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void processBindingConfiguration(String context, Item item, String bindingConfig) throws BindingConfigParseException {
+		super.processBindingConfiguration(context, item, bindingConfig);
+		SunnyWebBoxBindingConfig config = new SunnyWebBoxBindingConfig();
+		
+		String[] values = bindingConfig.split(",");
+		config.deviceId = values[0];
+		config.meta = values[1];
+		if(values.length>2)
+		{
+			config.urlKey = values[2];
+		}
+		
+		addBindingConfig(item, config);		
+	}
+	
+	
+	class SunnyWebBoxBindingConfig implements BindingConfig {
+		// put member fields here which holds the parsed values
+		String deviceId;
+		String meta;
+		String urlKey;
+		
+	}
+
+	@Override
+	public String getDeviceId(String itemName) {
+		SunnyWebBoxBindingConfig config = (SunnyWebBoxBindingConfig) bindingConfigs.get(itemName);
+		return config != null ? config.deviceId : null;
+	}
+	
+	@Override
+	public String getMeta(String itemName) {
+		SunnyWebBoxBindingConfig config = (SunnyWebBoxBindingConfig) bindingConfigs.get(itemName);
+		return config != null ? config.meta : null;
+	}
+	
+	@Override
+	public String getUrlKey(String itemName) {
+		SunnyWebBoxBindingConfig config = (SunnyWebBoxBindingConfig) bindingConfigs.get(itemName);
+		return config != null ? config.urlKey : null;
+	}
+	
+	
+}
---
+++ bundles/binding/org.openhab.binding.sunnywebbox/src/main/java/org/openhab/binding/sunnywebbox/requests/GetPlantOverviewRequest.java
@@ -0,0 +1,114 @@
+package org.openhab.binding.sunnywebbox.requests;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import net.minidev.json.JSONObject;
+
+import org.apache.commons.lang.math.NumberUtils;
+import org.openhab.binding.sunnywebbox.internal.SunnyWebBoxBinding;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.jayway.jsonpath.JsonPath;
+import static com.jayway.jsonpath.Criteria.*;
+import static com.jayway.jsonpath.Filter.*;
+//import com.jayway.restassured.RestAssured;
+//import com.jayway.restassured.path.json.JsonPath;
+
+import us.monoid.web.Content;
+import us.monoid.web.JSONResource;
+import us.monoid.web.Resty;
+
+public class GetPlantOverviewRequest {
+
+	String version;
+	String proc;
+	String id;
+	String format;
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(GetPlantOverviewRequest.class);
+
+	public GetPlantOverviewRequest() {
+		this.version = "1.0";
+		this.id = "1";
+		this.format = "JSON";
+		this.proc = "GetPlantOverview";
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getProc() {
+		return proc;
+	}
+
+	public void setProc(String proc) {
+		this.proc = proc;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFormat() {
+		return format;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+
+	public HashMap<String, Object> getValues(String URL) {
+		HashMap<String, Object> toRet = new HashMap<String, Object>();
+		Resty r = new Resty();
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(this);
+		String req = "RPC=" + json;
+		try {
+			JSONResource resource = r
+					.json("http://" + URL + "/rpc",
+							new Content("application/json", req.getBytes()));
+			
+			String response = resource.toObject().toString();
+			logger.debug("JSON received:{}", response);
+
+			List<Object> keyValues = JsonPath.read(response,
+					"$.result.overview[*]");
+			for (Object keyValue : keyValues) {
+				JSONObject jsonObjValue = (JSONObject) keyValue;
+				String meta = (String) jsonObjValue.get("meta");
+				meta = meta.replaceAll("\\.", "");
+				Object value = jsonObjValue.get("value");
+				if (meta.equals("Error"))
+					continue;
+
+				if (NumberUtils.isNumber(value.toString())) {
+					toRet.put(meta, Double.parseDouble(value.toString()));
+				} else {
+					toRet.put(meta, value);
+				}
+			}
+			toRet.put("updated", new Date());
+			return toRet;
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return toRet;
+		}
+	}
+}
